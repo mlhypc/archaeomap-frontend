1de466fc68ef7e7e8e7afb2a65fca4a1dba4f629
Melih <mlhypc@gmail.com>
2026-04-17 13:56:17 +0300

    JSX parse

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react';
 import cesium from 'vite-plugin-cesium';
 
 export default defineConfig({
-  plugins: [react(), cesium()],
+  plugins: [
+    react({ include: /\.(jsx|js|tsx|ts)$/ }),
+    cesium(),
+  ],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+  },
   server: {
     port: 3000,
   },
